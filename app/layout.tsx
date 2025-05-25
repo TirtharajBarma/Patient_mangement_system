f192a024d997d8342be4fbfbe2fb84b66bc9f6c7
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2025-05-26 02:35:40 +0530

    Fix: Updated tailwind.config.ts with backgroundImage fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from '@/lib/utils'
+import { ThemeProvider } from "@/components/theme-provider";
 
 // changes
 const fontSans = Plus_Jakarta_Sans({
@@ -23,7 +24,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>
-        {children}
+        <ThemeProvider                        // import themeProvider from shadcn npm dark
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
       </body>
     </html>
   );
