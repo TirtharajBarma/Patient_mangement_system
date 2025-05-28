ed6f20b2ce1e2fa37577b3e79d009cc331ce46ce
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2025-05-29 00:47:59 +0530

    add: Admin page Layout

---
+++ app/admin/page.tsx
@@ -0,0 +1,71 @@
+// "use client"
+
+import { DataTable } from '@/components/table/DataTable'
+import StatCard from '@/components/StatCard'
+import { getRecentAppointmentList } from '@/lib/actions/appointment.actions'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { columns } from '@/components/table/columns'
+
+
+const page = async() => {
+    const appointments = await getRecentAppointmentList();
+    // console.log('Appointments Data:', appointments.documents);
+
+  return (
+    <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
+      <header className='admin-header'>
+        <Link href='/' className='cursor-pointer'>
+            <Image 
+                src='/assets/icons/logo-full.svg'
+                height={32}
+                width={162}
+                alt='logo'
+                className='h-8 w-fit'
+            />
+        </Link>
+        <p className='text-16-semibold'>Admin Dashboard</p>
+      </header>
+
+      <main className='admin-main'>
+        <section className='w-full space-y-4'>
+            <h1 className='header'>Welcome</h1>
+            <p className='text-dark-700'>Start the day with managing new appointment</p>
+        </section>
+
+        {/* header section */}
+        <section className='admin-stat'>
+            <StatCard 
+                type = "appointments"
+                count = {appointments.scheduledCount}
+                label = 'Scheduled appointments'
+                icon = '/assets/icons/appointments.svg'
+            />
+
+            <StatCard 
+                type = "pending"
+                count = {appointments.pendingCount}
+                label = 'Pending appointments'
+                icon = '/assets/icons/pending.svg'
+            />
+
+            <StatCard 
+                type = "cancelled"
+                count = {appointments.cancelledCount}
+                label = 'Cancelled appointments'
+                icon = '/assets/icons/cancelled.svg'
+            />
+        </section>
+
+        {/* main section */}
+        {/* dataTable */}
+        <DataTable data={appointments.documents} columns={columns} />
+
+      </main>
+
+    </div>
+  )
+}
+
+export default page
