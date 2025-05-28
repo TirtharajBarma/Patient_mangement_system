e524f721252878b1b474ecc40b6196bf6afb1a64
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2025-05-29 03:24:21 +0530

    fix: background naming error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -94,8 +94,8 @@ const config = {
   		},
   		backgroundImage: {
 		appointments: `url('/assets/images/appointments-bg.png')`,
-		pending: `url('/assets/images/pending_bg.png')`,
-		cancelled: `url('/assets/images/cancelled_bg.png')`,
+		pending: `url('/assets/images/pending-bg.png')`,
+		cancelled: `url('/assets/images/cancelled-bg.png')`,
 		},
 
   		keyframes: {
